850ee60da40fba87b213bd988758814a3049ae1d
sofi-3107 <62622196+sofi-3107@users.noreply.github.com>
2022-09-01 15:31:05 -0300

    cors solved

--- src/main/java/com/uesiglo21/demo/controller/CafeteriaController.java
+++ src/main/java/com/uesiglo21/demo/controller/CafeteriaController.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uesiglo21.demo.entities.Pedido;
@@ -16,6 +18,7 @@ import com.uesiglo21.demo.repositories.ProductoRepository;
 import com.uesiglo21.demo.services.interfaces.PedidoService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class CafeteriaController {
 	
 	@Autowired
